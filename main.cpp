92e42eb903cbaa55d389d0f7b463022e2dcdb225
Hayden Roszell <roszellh@my.erau.edu>
2021-02-18 22:55:36 -0700

    Create output file function, leave comments. Completed

--- main.cpp
+++ main.cpp
@@ -1,3 +1,10 @@
+/*
+Project: Interleaver Cipher
+Description: Encrypt input file using interleaver cipher, and n columns, and output to file
+HISTORY: Verson 1.0 2/18/2021
+Copyright © 2021 Hayden Roszell. All rights reserved.
+*/
+
 #include <iostream>
 #include <string>
 #include <fstream>
@@ -9,18 +16,19 @@ bool write_to_file(const string& filename, const string& cipher_text);
 
 int main() {
     string plaintext = read_from_file("/Users/haydenroszell/Documents/School/College/ERAU/Sophomore/sem2/CS303/projects/project1/input.txt");
-    cout << plaintext << endl << "Enter # of columns for interleaver: ";
-    int num_of_columns; cin >> num_of_columns;
+    // Change file paths before you run on your computer
+    int num_of_columns;
+    cout << "Enter # of columns for interleaver: "; cin >> num_of_columns;
     string cipher_text = interleave(plaintext, num_of_columns);
-    if (write_to_file("/Users/haydenroszell/Documents/School/College/ERAU/Sophomore/sem2/CS303/projects/project1/output.txt", cipher_text))
-        cout << "Write succeeded" << endl;
-    else cout << "Write failed" << endl;
+
+    if (!write_to_file("/Users/haydenroszell/Documents/School/College/ERAU/Sophomore/sem2/CS303/projects/project1/output.txt", cipher_text))
+        return 1;
 
     return 0;
 }
 
 string read_from_file(const string& filename) {
-    fstream input (filename);
+    fstream input (filename); // ios::in
     string data;
     string line;
     if (input.is_open()) {
@@ -31,7 +39,7 @@ string read_from_file(const string& filename) {
     }
     else cout << "Something went wrong" << endl;
     for (int i = 0, length = data.size(); i < length; i++) {
-        if (ispunct(data[i]) || data[i] == ' ') {
+        if (ispunct(data[i]) || data[i] == ' ') { // remove punctuation & spaces
             data.erase(i--, 1);
             length = data.size();
         }
@@ -41,15 +49,13 @@ string read_from_file(const string& filename) {
 }
 
 string interleave(const string& plain_text, const int num_of_columns) {
-    int num_of_rows = ceil(plain_text.size() / (double)num_of_columns);
-
-    int count = 0;
-
+    int num_of_rows = ceil(plain_text.size() / (double)num_of_columns); int count = 0;
+    // Need to fill remainder of matrix with Z's
     auto *matrix = new char[num_of_columns*num_of_rows]; // large block of memory instead of dynamic # of column's
     for (int column = 0; column < num_of_columns; column++) {
         for (int row = 0; row < num_of_rows; row++) {
             if (count < plain_text.size()) {
-                matrix[row*num_of_columns+column] = plain_text[count]; // wee bit o' number theory
+                matrix[row*num_of_columns+column] = plain_text[count]; // wee bit o' math, everything else pretty easy
             } else matrix[row*num_of_columns+column] = 'Z';
             count++;
         }
@@ -65,10 +71,10 @@ string interleave(const string& plain_text, const int num_of_columns) {
 }
 
 bool write_to_file(const string& filename, const string& cipher_text) {
-    ofstream output (filename);
+    ofstream output (filename); // ios::out
     if (output.is_open()) {
-        for (int i = 0; i < cipher_text.size(); i++) {
-            output << cipher_text[i];
+        for (char i : cipher_text) {
+            output << i;
         }
         output.close();
         return true;
