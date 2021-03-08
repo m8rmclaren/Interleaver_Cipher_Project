37781d88d74e93ee9b17304a8a85d8b06895c02b
Hayden Roszell <roszellh@my.erau.edu>
2021-03-07 21:43:32 -0700

    Add write by rows/columns instead of just columns

--- main.cpp
+++ main.cpp
@@ -11,7 +11,8 @@ Copyright © 2021 Hayden Roszell. All rights reserved.
 using namespace std;
 
 string read_from_file(const string& filename);
-string interleave(const string& plain_text, int num_of_columns);
+string interleave_wbc(const string& plain_text, const int num_of_columns);
+string interleave_wbr(const string& plain_text, const int num_of_columns);
 bool write_to_file(const string& filename, const string& cipher_text);
 
 int main() {
@@ -19,7 +20,7 @@ int main() {
     // Change file paths before you run on your computer
     int num_of_columns;
     cout << "Enter # of columns for interleaver: "; cin >> num_of_columns;
-    string cipher_text = interleave(plaintext, num_of_columns);
+    string cipher_text = interleave_wbc(plaintext, num_of_columns);
 
     if (!write_to_file("/Users/haydenroszell/Documents/School/College/ERAU/Sophomore/sem2/CS303/projects/project1/output.txt", cipher_text))
         return 1;
@@ -47,8 +48,8 @@ string read_from_file(const string& filename) {
     }
     return data;
 }
-
-string interleave(const string& plain_text, const int num_of_columns) {
+// Interleave write by columns
+string interleave_wbc(const string& plain_text, const int num_of_columns) {
     int num_of_rows = ceil(plain_text.size() / (double)num_of_columns); int count = 0;
     // Need to fill remainder of matrix with Z's
     auto *matrix = new char[num_of_columns*num_of_rows]; // large block of memory instead of dynamic # of column's
@@ -61,9 +62,37 @@ string interleave(const string& plain_text, const int num_of_columns) {
         }
     }
     string cipher_text;
+    cout << plain_text << endl << matrix << endl;
     for (int row = 0; row < num_of_rows; row++) {
         for (int column = 0; column < num_of_columns; column++) {
             cipher_text.append(1, matrix[row*num_of_columns+column]);
+            cout << matrix[row*num_of_columns+column];
+        }
+    }
+
+    return cipher_text;
+}
+
+// Interleave write by rows
+string interleave_wbr(const string& plain_text, const int num_of_columns) {
+    int num_of_rows = ceil(plain_text.size() / (double)num_of_columns); int count = 0;
+    // Need to fill remainder of matrix with Z's
+    auto *matrix = new char[num_of_columns*num_of_rows]; // large block of memory instead of dynamic # of column's
+    for (int row = 0; row < num_of_rows; row++) {
+        for (int column = 0; column < num_of_columns; column++) {
+            if (count < plain_text.size()) {
+                matrix[row*num_of_columns+column] = plain_text[count]; // wee bit o' math, everything else pretty easy
+            } else matrix[row*num_of_columns+column] = 'Z';
+            count++;
+        }
+    }
+
+    string cipher_text;
+    cout << plain_text << endl << matrix << endl;
+    for (int column = 0; column < num_of_columns; column++) {
+        for (int row = 0; row < num_of_rows; row++) {
+            cipher_text.append(1, matrix[row*num_of_columns+column]);
+            cout << matrix[row*num_of_columns+column];
         }
     }
 
